c9e7c1507fff466431405de60924542fdd9f48ed
wpeery <peery@umail.ucsb.edu>
2019-02-10 09:09:50 -0800

    in the process of getting init to work

--- src/thsq-wrapper.ts
+++ src/thsq-wrapper.ts
@@ -1,55 +1,71 @@
-import * as thsq from 'thsq';
+import axios from 'axios'
+axios.defaults.withCredentials = true;
 import { Device, Devices, Attribute } from './index';
 
+const appKey = '0ac48bf3-9fab-4bad-8455-e394808eda6b'
+const baseUrl = `https://${appKey}.developer.thingsquare.com`
+
+
 class ThsqWrapper {
-  private thsq : thsq;
+  private apiKey : string;
+  private sessionCookie : string;
 
-  public constructor() {
-    this.thsq = thsq;
-  }
 
-  public disconnectFromServer() : void {
-    this.thsq.destroy();
-  }
-
-  public async init(apiKey : string) : Promise<Devices> {
-    let promise : Promise<Devices>;
-    promise = new Promise(resolve => this.thsq.init({ token: apiKey }, resolve));
-    const result = await promise;
-    if (result === undefined) {
+  public async init(apiKey : string) : Promise<string> {
+    const completeUrl2 = `${baseUrl}/0/session/`;
+    this.apiKey = apiKey;
+    const response = await axios.post(completeUrl2, {
+      form: {
+        token: this.apiKey
+      },
+      "headers": {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+    if (response.data != 'user-ok') {
       throw new Error('API key is incorrect');
     }
-    return result;
+    this.sessionCookie = response.headers['set-cookie'];
+    console.log(this.sessionCookie);
+    return response.data;
   }
 
-  public async getDevice(unique : string) : Promise<Device> {
-    return new Promise(resolve => this.thsq.getDevice(unique, resolve));
+  // public async getDevice(unique : string) : Promise<Device> {
+  //   return new Promise(resolve => this.thsq.getDevice(unique, resolve));
+  // }
+
+  public async getDeviceList() : Promise<any> {
+    const url = `${baseUrl}/0/devices/`
+    const response = await axios.get(url, {
+      headers : {
+        Cookie: this.sessionCookie
+      }
+    });
+    return response.data;
   }
 
-  public async getDeviceList() : Promise<Devices> {
-    return new Promise(resolve => this.thsq.getDevicelist(resolve));
-  }
+  // public async getVariable(unique : string,
+  //                          variableType : string,
+  //                          variableName : string) : Promise<Attribute> {
 
-  public async getVariable(unique : string,
-                           variableType : string,
-                           variableName : string) : Promise<Attribute> {
+  //   return new Promise(resolve => this.thsq.getVariable(unique,
+  //                                                       variableType,
+  //                                                       variableName,
+  //                                                       resolve));
+  // }
 
-    return new Promise(resolve => this.thsq.getVariable(unique,
-                                                        variableType,
-                                                        variableName,
-                                                        resolve));
-  }
-
-  public async getVariableHistory(unique : string,
-                                  variableType : string,
-                                  variableName : string,
-                                  options : object) : Promise<Attribute> {
-    return new Promise(resolve => this.thsq.getVariable(unique,
-                                                        variableType,
-                                                        variableName,
-                                                        resolve));
-  }
+  // public async getVariableHistory(unique : string,
+  //                                 variableType : string,
+  //                                 variableName : string,
+  //                                 options : object) : Promise<Attribute> {
+  //   return new Promise(resolve => this.thsq.getVariable(unique,
+  //                                                       variableType,
+  //                                                       variableName,
+  //                                                       resolve));
+  // }
 
 }
 
 export const thsqWrapperFactory = () => { return new ThsqWrapper(); };
+// curl -c session.txt -X POST --data token=483568031efa40638872947df118d22a https://0ac48bf3-9fab-4bad-8455-e394808eda6b.developer.thingsquare.com/0/session/
+// curl -b session.txt https://0ac48bf3-9fab-4bad-8455-e394808eda6b.developer.thingsquare.com/0/devices/
--- test/thsq-wrapper-init.test.ts
+++ test/thsq-wrapper-init.test.ts
@@ -5,30 +5,37 @@ require('dotenv').config();
 
 test('constructor for thsq-wrapper', () => {
   const wrapper = thsqWrapperFactory();
-  expect(wrapper).toHaveProperty('thsq');
-  wrapper.disconnectFromServer();
 });
 
 describe('Test init function', () => {
-  test('initialize, invalid api key', async () => {
-    expect.assertions(1);
-    const invalidApiKey = '1234';
-    const wrapper = thsqWrapperFactory();
-    try {
-      await wrapper.init(invalidApiKey);
-      fail();
-    } catch (e) {
-      expect(e.message).toBe('API key is incorrect');
-    }
-    wrapper.disconnectFromServer();
-  });
+  // test('invalid api key', async () => {
+  //   expect.assertions(1);
+  //   const invalidApiKey = '1234';
+  //   const wrapper = thsqWrapperFactory();
+  //   try {
+  //     await wrapper.init(invalidApiKey);
+  //     fail();
+  //   } catch (e) {
+  //     expect(e.message).toBe('API key is incorrect');
+  //   }
+  // });
 
-  test('initialize, valid api key', async () => {
-    expect.assertions(1);
+  // test('valid api key', async () => {
+  //   expect.assertions(1);
+  //   const apiKey = process.env.VALID_API_KEY;
+  //   const wrapper = thsqWrapperFactory();
+  //   const result = await wrapper.init(apiKey);
+  //   expect(result).toBe('user-ok');
+  // });
+
+  test('then get devices', async () => {
+    expect.assertions(2);
     const apiKey = process.env.VALID_API_KEY;
     const wrapper = thsqWrapperFactory();
     const result = await wrapper.init(apiKey);
-    expect(result).toBeInstanceOf(Object);
-    wrapper.disconnectFromServer();
+    expect(result).toBe('user-ok');
+    const devices = await wrapper.getDeviceList();
+    expect(devices).toBe({});
   });
+
 });
