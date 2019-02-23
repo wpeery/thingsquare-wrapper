a0567c617c99698660c5e34e5b70ce366ebcac0a
wpeery <peery@umail.ucsb.edu>
2019-02-22 21:37:10 -0800

    adds types

---
+++ dist/src/thsq-wrapper.d.ts
@@ -0,0 +1,11 @@
+import { Device, Devices, Attribute } from '../../src/index';
+export declare class ThsqWrapper {
+    private apiKey;
+    private sessionCookie;
+    init(apiKey: string): Promise<string>;
+    getDevice(id: string): Promise<Device>;
+    getDeviceList(): Promise<Devices>;
+    getVariable(id: string, variableType: string, variableName: string): Promise<Attribute>;
+    getVariableHistory(id: string, variableType: string, variableName: string, numDataPoints: string): Promise<Attribute[]>;
+}
+export declare const thsqWrapperFactory: () => ThsqWrapper;
